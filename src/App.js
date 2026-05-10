bddb739106fba7716cb1eb278d5e3ad15b15bf47
Prince Subedi <subediprince05@gmail.com>
2026-05-10 09:06:16 +0545

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 function App() {
-  let myVariable= "Hello";
   return (
     <>
-    <h3>My app</h3>
-    <p>{myVariable},MY App Works</p>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+
+        <a className="navbar-brand" href="/">Navbar</a>
+
+        <ul className="navbar-nav">
+
+          <li className="nav-item">
+            <a className="nav-link active" href="/">Home</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="/link">Link</a>
+          </li>
+
+          <li>
+            <hr className="dropdown-divider" />
+          </li>
+
+        </ul>
+
+      </div>
+    </nav>
     </>
   );
 }
