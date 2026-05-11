70f0fda7d0a81a072baca70e39d7388f6807d76b
Prince Subedi <subediprince05@gmail.com>
2026-05-11 19:19:02 +0545

    React Component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
+import Header from "./MyComponents/Header";
+
 function App() {
   return (
     <>
-   <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
+      <Header/>
+      <Todos/>
+      <Fotter/>
 
-    <a class="navbar-brand" href="#">Navbar</a>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-      {/* Left side: Home + About */}
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-      </ul>
-
-      {/* Right side: Search */}
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-
-    </div>
-  </div>
-</nav>
-   
     </>
   );
 }
