b17fcc71738d8aa53bf0a801b8c6531a2d0d94d6
Prince Subedi <subediprince05@gmail.com>
2026-05-12 17:29:49 +0545

    react component

--- src/App.js
+++ src/App.js
@@ -3,35 +3,33 @@ import Footer from "./MyComponents/Footer";
 import Todos from "./MyComponents/Todos";
 
 function App() {
-  let todo =[
-  {
-    sno: 1,
-    title:"Go to the market",
-    desc:"You need to go to the market to get this job done1"
-  },
+  let todo = [
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to the market to get this job done1"
+    },
 
-  {
-    sno: 1,
-    title:"Go to the mall",
-    desc:"You need to go to the market to get this job done2"
-  },
+    {
+      sno: 1,
+      title: "Go to the mall",
+      desc: "You need to go to the market to get this job done2"
+    },
 
-  {
-    sno: 1,
-    title:"Go to the Hotel",
-    desc:"You need to go to the market to get this job3 "
-  },
+    {
+      sno: 1,
+      title: "Go to the Hotel",
+      desc: "You need to go to the market to get this job3 "
+    },
   ]
+
   return (
     <>
-      <Header title="My Todos list" searchBar={true}/>
-      <Todos/>
-      <Footer/>
-
-
+      <Header title="My Todos list" searchBar={true} />
+      <Todos todos={todo} />
+      <Footer />
     </>
   );
 }
 
 export default App;
-
